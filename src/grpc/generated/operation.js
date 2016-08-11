c831dd1b20cfac3c836e33bdeb54da032f3b7848
Hui Si Goh <huisigoh@gmail.com>
2016-08-11 08:45:02 +0800

    create calculator proto

---
+++ src/grpc/generated/operation.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.Operation');
+
+/**
+ * @enum {number}
+ */
+proto.Operation = {
+  ADD: 0,
+  SUBTRACT: 1,
+  MULTIPLY: 2,
+  DIVIDE: 3
+};
+
