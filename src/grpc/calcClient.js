0900482c223cf18791e628289abc963cd4b3e2f1
Hui Si Goh <hui.si.goh@sap.com>
2016-08-12 09:45:27 +0800

    fix bug

--- src/grpc/calcClient.js
+++ src/grpc/calcClient.js
@@ -1,6 +1,4 @@
 var PROTO_CALC_PATH = __dirname + '/proto/Calculator.proto';
-var Work = require('./generated/work');
-var OPERATION = require('./generated/operation');
 
 var grpc = require('grpc');
 var calc_proto = grpc.load(PROTO_CALC_PATH).calculator;
@@ -8,26 +6,17 @@ var calc_proto = grpc.load(PROTO_CALC_PATH).calculator;
 function main() {
   var client = new calc_proto.Calculator('localhost:12346',
                                        grpc.credentials.createInsecure());
-//   var user;
-//   if (process.argv.length >= 3) {
-//     user = process.argv[2];
-//   } else {
-//     user = 'world';
-//   }
 
-  var workReq = new Work();
+  var workReq = new calc_proto.Work();
   workReq.logid=123456;
   workReq.num1=4;
   workReq.num2=8;
-  workReq.op=OPERATION.ADD; 
+  workReq.op= calc_proto.Operation.ADD; 
   workReq.comment='TEST_COMMENT';
 
   client.calculate(workReq, function(err, response) {
     console.log('Calculated:', response.res);
   });
-
-
-
 }
 
 main();
--- src/grpc/client.js
+++ src/grpc/client.js
@@ -1,18 +1,18 @@
 var PROTO_PATH = __dirname + '/helloworld.proto';
-var CALC_PATH = __dirname + '/Calculator.proto';
-var Work = require("./generated/work");
-var OPERATION = require("./generated/operation");
+// var CALC_PATH = __dirname + '/Calculator.proto';
+// var Work = require("./generated/work");
+// var OPERATION = require("./generated/operation");
 
 var grpc = require('grpc');
 var hello_proto = grpc.load(PROTO_PATH).helloworld;
-var calculator_proto = grpc.load(CALC_PATH).calculator
+// var calculator_proto = grpc.load(CALC_PATH).calculator
 
 function main() {
   var client = new hello_proto.Greeter('localhost:12345',
                                        grpc.credentials.createInsecure());
 
-  var calc_client = new calculator_proto.Calculator('localhost:12346',
-                                       grpc.credentials.createInsecure());                              
+  // var calc_client = new calculator_proto.Calculator('localhost:12346',
+  //                                      grpc.credentials.createInsecure());                              
                                        
   var user;
   if (process.argv.length >= 3) {
@@ -27,24 +27,24 @@ function main() {
   });
 
   //params: Work, callback
-  var workReq = new Work();
+  // var workReq = new Work();
 
-  workReq.logid =54321;
-  workReq.num1 = 4;
-  workReq.num2 = 8;
-  workReq.op = OPERATION.ADD;
-  workReq.comment = 'TEST_COMMENT';
+  // workReq.logid =54321;
+  // workReq.num1 = 4;
+  // workReq.num2 = 8;
+  // workReq.op = OPERATION.ADD;
+  // workReq.comment = 'TEST_COMMENT';
 
 
-  calc_client.Calculate(workReq, function(err, response){
-    if(!err){
-      console.log('Calculate: ', response.message);
-    }
-    else{
-      console.log(err);
-    }
+  // calc_client.Calculate(workReq, function(err, response){
+  //   if(!err){
+  //     console.log('Calculate: ', response.message);
+  //   }
+  //   else{
+  //     console.log(err);
+  //   }
     
-  });
+  // });
 
 }
 
