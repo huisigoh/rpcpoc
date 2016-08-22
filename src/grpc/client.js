cf8587dbba1e77dba04ec1eee631074588c153b6
Hui Si Goh <hui.si.goh@sap.com>
2016-08-22 17:16:41 +0800

    update example

--- src/grpc/client.js
+++ src/grpc/client.js
@@ -1,51 +1,66 @@
-var PROTO_PATH = __dirname + '/helloworld.proto';
-// var CALC_PATH = __dirname + '/Calculator.proto';
-// var Work = require("./generated/work");
-// var OPERATION = require("./generated/operation");
+var PROTO_PATH = __dirname + '/proto/helloworld.proto';
 
 var grpc = require('grpc');
 var hello_proto = grpc.load(PROTO_PATH).helloworld;
-// var calculator_proto = grpc.load(CALC_PATH).calculator
 
-function main() {
-  var client = new hello_proto.Greeter('localhost:12345',
-                                       grpc.credentials.createInsecure());
+var client = new hello_proto.Greeter('localhost:12345', grpc.credentials.createInsecure());      
 
-  // var calc_client = new calculator_proto.Calculator('localhost:12346',
-  //                                      grpc.credentials.createInsecure());                              
-                                       
-  var user;
-  if (process.argv.length >= 3) {
-    user = process.argv[2];
-  } else {
-    user = 'world';
-  }
+function runSayHello(){
+  client.sayHello({name: 'testname'}, function(err, response) {
+    console.log('Client::Greeting:', response.message);
+  });
+}
 
-  //params: HelloRequest, callback
-  client.sayHello({name: user}, function(err, response) {
-    console.log('Greeting:', response.message);
+function runChattyClient(){
+  var call = client.chattyClient(function(err, response){
+    console.log('Client::Stream message sent');
   });
 
-  //params: Work, callback
-  // var workReq = new Work();
-
-  // workReq.logid =54321;
-  // workReq.num1 = 4;
-  // workReq.num2 = 8;
-  // workReq.op = OPERATION.ADD;
-  // workReq.comment = 'TEST_COMMENT';
-
-
-  // calc_client.Calculate(workReq, function(err, response){
-  //   if(!err){
-  //     console.log('Calculate: ', response.message);
-  //   }
-  //   else{
-  //     console.log(err);
-  //   }
-    
-  // });
-
+  setInterval(function(){
+      call.write({name: 'testname' + new Date()});
+  },500);
 }
+
+function runChattyServer(){
+  var call = client.chattyServer();
+  call.on('data', function(response){
+    console.log('Client::Response from server: ' + response);
+  });
+
+  call.on('end', function(){
+    call.end();
+  });
+}
+
+function runChat(){
+  var call = client.everyonesChatty();
+
+  call.on('data', function(){
+    console.log('Client::Message from server: ' + response);
+  })
+
+
+  setInterval(function(){
+        call.write({name: 'testname' + new Date()});
+  },500);
+
+
+  call.end();
+}
+
+function main(){
+  if(process.argv.length != 3){
+    console.error('Client::Op num required');
+  }
+  else{
+    switch(process.argv[2]){
+      case '1': runSayHello(); break;
+      case '2': runChattyClient(); break;
+      case '3': runChattyServer(); break;
+      case '4': runSayHello(); break;
+    }
+  }
+}
+
 
 main();
--- src/grpc/server.js
+++ src/grpc/server.js
@@ -1,9 +1,7 @@
-var PROTO_PATH = __dirname + '/helloworld.proto';
-var CALC_PATH = __dirname + '/Calculator.proto';
+var PROTO_PATH = __dirname + '/proto/helloworld.proto';
 
 var grpc = require('grpc');
 var hello_proto = grpc.load(PROTO_PATH).helloworld; //proto.package
-var calculator_proto = grpc.load(CALC_PATH).calculator
 
 /**
  * Implements the SayHello RPC method.
@@ -12,44 +10,40 @@ function sayHello(call, callback) {
   callback(null, {message: 'Hello ' + call.request.name});
 }
 
-function calculate(call, callback){
+function chattyClient(call, callback){
+  var requests;
+  call.on('data', function(req){
+    console.log('Reading from Chatty Client: ' + req.name)
+    requests+=req.name + ', '
+  });
 
-  console.log("calculating: " + call.logid);
-
-    // var val = 0;
-    // if (work.op == ttypes.Operation.ADD) {
-    //   val = work.num1 + work.num2;
-    // } else if (work.op === ttypes.Operation.SUBTRACT) {
-    //   val = work.num1 - work.num2;
-    // } else if (work.op === ttypes.Operation.MULTIPLY) {
-    //   val = work.num1 * work.num2;
-    // } else if (work.op === ttypes.Operation.DIVIDE) {
-    //   if (work.num2 === 0) {
-    //     var x = new ttypes.InvalidOperation();
-    //     x.whatOp = work.op;
-    //     x.why = 'Cannot divide by 0';
-    //     result(x);
-    //     return;
-    //   }
-    //   val = work.num1 / work.num2;
-    // } else {
-    //   var x = new ttypes.InvalidOperation();
-    //   x.whatOp = work.op;
-    //   x.why = 'Invalid operation';
-    //   result(x);
-    //   return;
-    // }
-
-    // var entry = new SharedStruct();
-    // entry.key = logid;
-    // entry.value = ""+val;
-    // data[logid] = entry;
-
-    // result(null, val);
-    
-  callback(null, {res: 32});
+  call.on('end', function(){
+    callback(null, {message: 'Results read: ' + requests});
+  });
 }
 
+function chattyServer(call){
+  for(var i = 0; i < 10; i++){
+    setTimeout(function(){
+          call.write({message: 'StreamingServer: ' + call.request.name + ' : ' + i});
+    },500);
+  }
+  call.end();
+}
+
+function everyonesChatty(){
+  call.on('data', function(req){
+    for(var i = 0; i < 10; i++){
+      setTimeout(function(){
+        call.write({message: 'Server - Bi-Directional : ' + call.request.name + ' : ' + i});
+      },500);
+    }
+  });
+
+  call.on('end', function(){
+    call.end();
+  });
+}
 
 /**
  * Starts an RPC server that receives requests for the Greeter service at the
@@ -57,11 +51,13 @@ function calculate(call, callback){
  */
 function main() {
   var server = new grpc.Server();
-  var calc_server = new grpc.Server();
-  server.addProtoService(hello_proto.Greeter.service, {sayHello: sayHello});
-  calc_server.addProtoService(calculator_proto.Calculator.service, {Calculate: Calculate});
+  server.addProtoService(hello_proto.Greeter.service, {
+    sayHello: sayHello,
+    chattyClient: chattyClient,
+    chattyServer: chattyServer,
+    everyonesChatty: everyonesChatty
+  });
   server.bind('0.0.0.0:12345', grpc.ServerCredentials.createInsecure());
-  calc_server.bind('0.0.0.0:12346', grpc.ServerCredentials.createInsecure());
   server.start();
 }
 
